6c1a57b0c4c337be64971ba15b78863985fee3ee
mantkowicz <antek15@gmail.com>
2015-06-15 18:56:43 +0200

    atlas + zoom tylko w telefonach

--- src/com/apptogo/runner/android/AndroidLauncher.java
+++ src/com/apptogo/runner/android/AndroidLauncher.java
@@ -3,6 +3,7 @@ package com.apptogo.runner.android;
 import android.os.Bundle;
 
 import com.apptogo.runner.main.Runner;
+import com.apptogo.runner.vars.Box2DVars;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
@@ -10,6 +11,9 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		Box2DVars.ZOOM_CAMERA_IN_REGISTER_SCREEN = true;
+		
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
         config.useCompass = false;
